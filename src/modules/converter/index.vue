784f4f62577d567da251213d0168f991e156a917
Kravchuk Roman <romakravchuk1995@gmail.com>
2021-06-22 18:41:42 +0300

    Remove unused elements / Refactore code

--- src/modules/converter/index.vue
+++ src/modules/converter/index.vue
@@ -6,16 +6,10 @@
     <div class="converter-top">
       <div class="converter-top__left" v-if="valutes">
         <v-select :options="countries" v-model="selected[0]" :clearable="false" @input="convert" ></v-select>
-<!--        <select name="country" id="country" v-model="selected[0]" @change="convert">-->
-<!--          <option :value="item" v-for="(item,id) in countries" :key="id">{{ item }}</option>-->
-<!--        </select>-->
         <input @input="convert" v-model="inputed" type="number">
       </div>
       <div class="converter-top__right">
         <v-select :options="countries" v-model="selected[1]" :clearable="false" @input="convert" ></v-select>
-<!--        <select name="country" id="country1" @change="convert" v-model="selected[1]" :items="countries">-->
-<!--          <option :value="item" v-for="(item,id) in countries" :key="id">{{ item }}</option>-->
-<!--        </select>-->
         <input type="text" v-model="result" readonly>
       </div>
     </div>
